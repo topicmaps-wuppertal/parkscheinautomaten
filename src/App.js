c34f5e35aa50f10df8ab51e2066e1081be8259bc
helllth <th@cismet.de>
2021-02-19 09:19:51 +0100

    corrected a typo in an url

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const getGazData = async (setGazData) => {
 
   sources.adressen = await md5FetchText(prefix, host + "/data/adressen.json");
   sources.bezirke = await md5FetchText(prefix, host + "/data/bezirke.json");
-  sources.quartiere = await md5FetchText(prefix, host + "data/quartiere.json");
+  sources.quartiere = await md5FetchText(prefix, host + "/data/quartiere.json");
   sources.pois = await md5FetchText(prefix, host + "/data/pois.json");
   sources.kitas = await md5FetchText(prefix, host + "/data/kitas.json");
 
